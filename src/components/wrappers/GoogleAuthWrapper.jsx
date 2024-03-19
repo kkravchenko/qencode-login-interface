a15673a00b4170621cdbdf580384b8d35a94d3f1
Konstantin Kravchenko <k.kravchenko@inits.com.ua>
2024-03-19 12:12:55 +0200

    Move google client id to env

--- src/components/wrappers/GoogleAuthWrapper.jsx
+++ src/components/wrappers/GoogleAuthWrapper.jsx
@@ -1,7 +1,7 @@
 import { GoogleOAuthProvider } from '@react-oauth/google'
 
 export const GoogleAuthWrapper = ({ children }) => (
-  <GoogleOAuthProvider clientId='466237624909-pt827s5sr1aacrksv2d5v6d157r284u3'>
+  <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
     {children}
   </GoogleOAuthProvider>
 )
